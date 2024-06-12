df247d0417d9ccd77dc6431d5a93ca721b365e54
Muhammad4340 <68977832+Muhammad4340@users.noreply.github.com>
2024-06-12 10:28:14 +0700

    Update stream-kankerpayudara.py

--- stream-kankerpayudara.py
+++ stream-kankerpayudara.py
@@ -6,7 +6,7 @@ import streamlit as st
 model = pickle.load(open('penyakit_kankerpayudara.sav','rb'))
 
 # judul web
-st.title('Klasifikasi Penyakit Kanker Payudara')
+st.title('Klasifikasi Penyakit Kanker Payudara dengan LogisticRegression')
 
 col1, col2, col3 = st.columns(3)
 
